5f1507d01d28ac7ab986855cab416cc7b69dbc5c
Brian Firestone <bfirestone2339@gmail.com>
2021-02-21 13:37:00 -0500

    Add #get_data_by_title

--- app/models/services/omdb_service.rb
+++ app/models/services/omdb_service.rb
@@ -1,7 +1,9 @@
 class OmdbService
 
     def get_data_by_title(title)
-        response = HTTP.
+        response = HTTP.get("http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&t=#{title}").to_s
+        parsed_response = JSON.parse(response)
+        parsed_response
     end
 
 end
