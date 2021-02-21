eee25338cd85d1e25e6eab958d974ae017a4afaf
Brian Firestone <bfirestone2339@gmail.com>
2021-02-21 11:41:36 -0500

    Establish model associations

--- app/models/genres_movie.rb
+++ app/models/genres_movie.rb
@@ -1,2 +1,4 @@
 class GenresMovie < ApplicationRecord
+    belongs_to :genres
+    belongs_to :movies
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+    has_many :genres, through: :genres_movies
+    has_many :reviews
 end
