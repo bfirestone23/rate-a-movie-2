46168b485fe90cec728fa687b998bea5b98c75dd
Brian Firestone <bfirestone2339@gmail.com>
2021-02-21 16:55:29 -0500

    Implement create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,13 @@
+require_relative "../models/services/omdb_service.rb"
+require 'uri'
+
 class MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
 
     def show
-        @movie = Movie.find_by(params[:id])
+        @movie = Movie.find_by_id(params[:id])
     end
 
     def new
@@ -15,8 +18,14 @@ class MoviesController < ApplicationController
         @movie = Movie.new(movie_params)
 
         escaped_title = URI.escape(@movie.title)
-        hash = OmdbService.new.get_all_data_by_title(escaped_title)
-        binding.pry
+
+        hash = OmdbService.new
+        movie_data = hash.get_all_data_by_title(escaped_title)
+        genres = hash.parse_genres
+
+        @movie.update(movie_data)
+
+        redirect_to movie_path(@movie)
     end
 
     def edit
@@ -28,6 +37,6 @@ class MoviesController < ApplicationController
     private
 
     def movie_params
-        params.require(:movie).permit(:title, :release_date, :run_time, :director, :plot, :awards, :poster, :imdb_rating)
+        params.require(:movie).permit(:title)
     end
 end
