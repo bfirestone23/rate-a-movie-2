649f15ad1338645a922c31bbdb0ffe0463721a59
Brian Firestone <bfirestone2339@gmail.com>
2021-02-21 16:56:04 -0500

    Implement #parse_genres and finish implementing #get_all_data_by_title

--- app/models/services/omdb_service.rb
+++ app/models/services/omdb_service.rb
@@ -4,6 +4,22 @@ class OmdbService
         response = HTTP.get("http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&t=#{title}").to_s
         parsed_response = JSON.parse(response)
         parsed_response
+        filtered_response = parsed_response.select do |a|
+            a == "Title" ||
+            a == "Released" ||
+            a == "Runtime" ||
+            a == "Director" ||
+            a == "Plot" ||
+            a == "Awards" ||
+            a == "Poster" ||
+            a == "imdbRating"
+        end
+        @genres = parsed_response["Genre"]
+        movie_attributes = filtered_response.deep_transform_keys(&:downcase).symbolize_keys
+    end
+
+    def parse_genres
+        @genres.split(', ').map {|g| g}
     end
 
 end
