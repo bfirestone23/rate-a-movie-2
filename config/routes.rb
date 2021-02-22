d8da445ee2d7e530074593e572426091729e40a7
Brian Firestone <bfirestone2339@gmail.com>
2021-02-21 19:02:04 -0500

    Remove /users/new route, add /signup, /login, /logout, /home routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :genres
   resources :movies
   resources :reviews
-  resources :users
+
+  resources :users, except: [:new]
+  get '/signup', to: 'users#new', as: 'new_user'
+  get '/login', to: 'users#new_login', as: 'login'
+  post '/login', to: 'users#create_login'
+  get '/logout', to: 'users#logout', as: 'logout'
+  get '/home', to: 'application#home', as: 'home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
