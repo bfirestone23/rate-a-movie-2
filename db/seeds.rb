4378cb7ff0931cc72c4284310a015a8f342ef750
Brian Firestone <bfirestone2339@gmail.com>
2021-02-22 11:45:56 -0500

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
 
-movies = Movie.create([{ title: "Star Wars"}, { title: "Titanic" }, { title: "Lord of the Rings" }])
-genres = Genre.create([{name: "Drama"}, {name: "Comedy"}, {name: "Sci-Fi"}, {name: "Fantasy"}])
-User.create(username: "brian", email: "bfirestone2339@gmail.com", password: "test")
+User.create(username: "Brian", email: "bfirestone2339@gmail.com", password: "test")
+User.create(username: "Michael", email: "bfirestone2339@gmail.com", password: "test")
+User.create(username: "Jordan", email: "bfirestone2339@gmail.com", password: "test")
+User.create(username: "Andrew", email: "bfirestone2339@gmail.com", password: "test")
+User.create(username: "Marcus", email: "bfirestone2339@gmail.com", password: "test")
+
+
+
+
+table = CSV.parse(File.read("lib/assets/movies.csv"), headers: true)
+
+failures = []
+
+table.each do |r|
+    movie = Movie.create(title: r)
+
+    escaped_title = URI.escape(movie.title)
+    
+
+    hash = OmdbService.new
+    result = hash.get_all_data_by_title(escaped_title)
+
+    movie.update(result)
+    
+    if movie.released == nil
+        failures << movie
+    end
+end
+
+failures.each do |movie|
+    puts movie.title
+end
+
+reviews_table = CSV.parse(File.read("lib/assets/reviews.csv"), headers: true)
+
+review_failures = []
+reviews_table.each do |r|
+    review = Review.create(rating: r[2].to_f, notes: r[4], watch_date: r[3])
+    review.user = User.find_by(username: r[0])
+    review.movie = Movie.find_by(title: r[1])
+    review.save
+
+    if review.movie == nil
+        review.destroy
+    end
+end
