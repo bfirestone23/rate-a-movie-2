2090814235cbe25717cdd136b4fec51dcf309026
Brian Firestone <bfirestone2339@gmail.com>
2021-02-21 19:00:28 -0500

    Update .find_by in show action, add erb render in new action, implement create action, remove edit and update actions, add and implement new_login, create_login, and logout actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,40 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find_by(params[:id])
+        @user = User.find_by_id(params[:id])
     end
 
     def new
         @user = User.new
+        render :signup
     end
 
     def create
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
-    def edit
+    def new_login
+        @user = User.new
     end
 
-    def update
+    def create_login
+        @user = User.find_by(username: params.require(:user).permit(:username))
+        redirect_to user_path(@user)
+    end
+
+    def logout
+        if logged_in?
+            session.destroy
+            redirect_to home_path
+        else
+            redirect_to login_path
+        end
     end
 
     private
