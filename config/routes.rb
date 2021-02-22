97b12e70cd2d266712e69679a013ae84f861f072
Brian Firestone <bfirestone2339@gmail.com>
2021-02-22 11:45:32 -0500

    Update GET and POST /login route names for clarity, remove unused genres and movies routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :genres
-  resources :movies
+  resources :genres, only: [:index, :show]
+  resources :movies, except: [:edit, :update]
   resources :reviews
 
   resources :users, except: [:new]
+
   get '/signup', to: 'users#new', as: 'new_user'
-  get '/login', to: 'users#new_login', as: 'login'
-  post '/login', to: 'users#create_login'
+  get '/login', to: 'users#get_login', as: 'login'
+  post '/login', to: 'users#post_login'
   get '/logout', to: 'users#logout', as: 'logout'
   get '/home', to: 'application#home', as: 'home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
